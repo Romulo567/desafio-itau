99fed4d8e9816c986de1495538f8513c68171ff6
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-24 10:23:29 -0400

    feat(TransacaoService): Adiciona lógica de negocio para criar transação

--- src/main/java/com/desafioitau/business/services/TransacaoService.java
+++ src/main/java/com/desafioitau/business/services/TransacaoService.java
@@ -1,11 +1,38 @@
 package com.desafioitau.business.services;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.desafioitau.controller.dtos.TransacaoRequestDTO;
+import com.desafioitau.exceptions.UnprocessableEntity;
+
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TransacaoService {
 
+	private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
+	
+	public void criarTransacoes(TransacaoRequestDTO dto) {
+		
+		log.info("Iniciado o processamento de criar transacoes");
+		
+		if(dto.dataHora().isAfter(OffsetDateTime.now())) {
+			log.error("Data e hora maiores que a data atual");
+			throw new UnprocessableEntity("Data e hora maiores que a data e hora atual");
+		}
+		
+		if(dto.valor()< 0) {
+			log.error("Valor não pode ser menor que 0");
+			throw new UnprocessableEntity("Valor não pode ser menor que 0");
+		}
+		
+		listaTransacoes.add(dto);
+	}
 }
