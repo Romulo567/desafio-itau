8b67649dd2e78d5b90f88f89887393f5a2cd79a6
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-24 09:58:25 -0400

    feat: Adiciona ClienteService

---
+++ src/main/java/com/desafioitau/business/services/TransacaoService.java
@@ -0,0 +1,11 @@
+package com.desafioitau.business.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TransacaoService {
+
+}
