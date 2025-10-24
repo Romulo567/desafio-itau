1674134b1489f107c476190130d7c4d15cdb43ab
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-24 09:44:11 -0400

    feat: Configuração inicial do projeto

---
+++ src/main/java/com/desafioitau/api_desafio_itau/ApiDesafioItauApplication.java
@@ -0,0 +1,13 @@
+package com.desafioitau.api_desafio_itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesafioItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesafioItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafioitau/api_desafio_itau/ApiDesafioItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioitau.api_desafio_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesafioItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
