45c27a2ee9fc70bcb59209d10f959a658ba4ecf1
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-27 10:41:15 -0400

    feat(EstatisticasService): Criado a lógica para calcular estatisticas de transação

--- src/main/java/com/desafioitau/business/services/EstatisticasService.java
+++ src/main/java/com/desafioitau/business/services/EstatisticasService.java
@@ -1,6 +1,12 @@
 package com.desafioitau.business.services;
 
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.desafioitau.controller.dtos.EstatisticasResponseDTO;
+import com.desafioitau.controller.dtos.TransacaoRequestDTO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +15,17 @@ import lombok.RequiredArgsConstructor;
 public class EstatisticasService {
 
 	private final TransacaoService transacaoService;
+	
+	public EstatisticasResponseDTO calcularEstatisticasTransacoes(Integer intervaloBusca) {
+		List<TransacaoRequestDTO> transacoes = transacaoService.buscarTransacoes(intervaloBusca);
+		
+		DoubleSummaryStatistics estatisticasTransacoes = transacoes.stream()
+				.mapToDouble(TransacaoRequestDTO::valor).summaryStatistics();
+		
+		return new EstatisticasResponseDTO(estatisticasTransacoes.getCount(), 
+				estatisticasTransacoes.getSum(), 
+				estatisticasTransacoes.getAverage(),
+				estatisticasTransacoes.getMin(),
+				estatisticasTransacoes.getMax());
+	}
 }
