2c8aa862d9c32ad1cec656548ee52fd87cddeaa9
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-28 10:18:37 -0400

    feat: Adiciona EndPoint para adicionar transacao

--- src/main/java/com/desafioitau/controller/TransacaoController.java
+++ src/main/java/com/desafioitau/controller/TransacaoController.java
@@ -1,10 +1,14 @@
 package com.desafioitau.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.desafioitau.controller.dtos.TransacaoRequestDTO;
+import com.desafioitau.services.TransacaoService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +17,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TransacaoController {
 
+	private final TransacaoService transacaoService;
 	
+	@PostMapping
+	public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoRequestDTO dto){
+		
+		transacaoService.criarTransacoes(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 
 }
