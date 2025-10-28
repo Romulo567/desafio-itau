3b698c1cfdcc543b13a52d9cb4d55a7cc5977b23
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-28 10:24:50 -0400

    feat: Criado a classe EstatisticasController

---
+++ src/main/java/com/desafioitau/controller/EstatisticasController.java
@@ -0,0 +1,14 @@
+package com.desafioitau.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1/estatistica")
+@RequiredArgsConstructor
+public class EstatisticasController {
+
+	
+}
