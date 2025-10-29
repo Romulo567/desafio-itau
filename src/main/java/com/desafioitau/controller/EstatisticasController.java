3f524aa3afcd8d4df5f6b31c16e27e04bb1b7359
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-29 10:42:35 -0400

    fix: Adiciona @GetMapping ausente no método buscarEstatisticas

--- src/main/java/com/desafioitau/controller/EstatisticasController.java
+++ src/main/java/com/desafioitau/controller/EstatisticasController.java
@@ -1,6 +1,7 @@
 package com.desafioitau.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class EstatisticasController {
 
 	private final EstatisticasService estatisticasService;
 	
+	@GetMapping
 	public ResponseEntity<EstatisticasResponseDTO> buscarEstatisticas(
 			@RequestParam(value = "intervaloBusca", required = false , defaultValue = "60") Integer intervaloBusca){
 		
