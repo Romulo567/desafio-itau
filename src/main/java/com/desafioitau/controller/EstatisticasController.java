ad8c6d800b19f98d3c2eceee6d986106e12273f0
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-29 10:44:31 -0400

    docs: Adiciona documentação para o endpoint de buscarEstatisticas

--- src/main/java/com/desafioitau/controller/EstatisticasController.java
+++ src/main/java/com/desafioitau/controller/EstatisticasController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.desafioitau.controller.dtos.EstatisticasResponseDTO;
 import com.desafioitau.services.EstatisticasService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,6 +22,12 @@ public class EstatisticasController {
 	private final EstatisticasService estatisticasService;
 	
 	@GetMapping
+	@Operation(description = "Recurso responsável por buscar estatisticas de transações")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Busca efetuada com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Erro na busca de estatisticas de transações"),
+			@ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+	})
 	public ResponseEntity<EstatisticasResponseDTO> buscarEstatisticas(
 			@RequestParam(value = "intervaloBusca", required = false , defaultValue = "60") Integer intervaloBusca){
 		
