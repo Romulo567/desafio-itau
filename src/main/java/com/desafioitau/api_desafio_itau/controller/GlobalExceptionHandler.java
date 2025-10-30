92c68b7413a957d7645c7acf451cfb8384411806
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-30 10:00:28 -0400

    feat: Adiciona tratamento para Internal Server Error

--- src/main/java/com/desafioitau/api_desafio_itau/controller/GlobalExceptionHandler.java
+++ src/main/java/com/desafioitau/api_desafio_itau/controller/GlobalExceptionHandler.java
@@ -16,4 +16,10 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<String> handleUnprocessableEntity(UnprocessableEntity e){
 		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Erro: " + e.getMessage());
 	}
+	
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public ResponseEntity<String> handleException(Exception e){
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + e.getMessage());
+	}
 }
