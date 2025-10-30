9da5b96543becfca6609aa5c2dd61ba1225d121b
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-30 09:57:06 -0400

    feat: Criado a classe GlobalExceptionHandler

---
+++ src/main/java/com/desafioitau/api_desafio_itau/controller/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.desafioitau.api_desafio_itau.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.desafioitau.api_desafio_itau.exceptions.UnprocessableEntity;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	
+}
