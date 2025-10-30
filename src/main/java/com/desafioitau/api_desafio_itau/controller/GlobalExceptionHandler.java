8a553363b4bc7ce7d39690c37ed6558a055cb810
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-10-30 09:57:54 -0400

    feat: Adiciona o tratamento para UnprocessableEntity

--- src/main/java/com/desafioitau/api_desafio_itau/controller/GlobalExceptionHandler.java
+++ src/main/java/com/desafioitau/api_desafio_itau/controller/GlobalExceptionHandler.java
@@ -11,5 +11,9 @@ import com.desafioitau.api_desafio_itau.exceptions.UnprocessableEntity;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-	
+	@ExceptionHandler(UnprocessableEntity.class)
+	@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+	public ResponseEntity<String> handleUnprocessableEntity(UnprocessableEntity e){
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Erro: " + e.getMessage());
+	}
 }
