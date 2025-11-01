76b90449670bba30944debc2158e516b32e9c6ea
Romulo567 <romulowagnes99@gmail.com>
2025-11-01 10:15:15 -0400

    test: Adiciona teste para adicionar transacao com sucesso

--- src/test/java/com/desafioitau/api_desafio_itau/services/TransacaoServiceTest.java
+++ src/test/java/com/desafioitau/api_desafio_itau/services/TransacaoServiceTest.java
@@ -1,18 +1,15 @@
 package com.desafioitau.api_desafio_itau.services;
 
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.OffsetDateTime;
-import java.util.Collections;
+import java.util.List;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.desafioitau.api_desafio_itau.controller.dtos.EstatisticasResponseDTO;
@@ -34,4 +31,12 @@ public class TransacaoServiceTest {
 		estatisticas = new EstatisticasResponseDTO(1L, 20.0, 20.0, 20.0, 20.0);
 	}
 	
+	@Test
+	void deveAdicionarTransacaoComSucesso() {
+		transacaoService.criarTransacoes(transacao);
+		
+		List<TransacaoRequestDTO> transacoes = transacaoService.buscarTransacoes(5000);
+		
+		assertTrue(transacoes.contains(transacao));
+	}
 }
