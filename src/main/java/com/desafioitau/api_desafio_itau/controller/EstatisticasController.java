979b85b2005e85c2f477060d07b8723db3ba04cc
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-11-04 11:11:19 -0400

    fix: Adiciona '/' aos paths de endpoints

--- src/main/java/com/desafioitau/api_desafio_itau/controller/EstatisticasController.java
+++ src/main/java/com/desafioitau/api_desafio_itau/controller/EstatisticasController.java
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Estatisticas", description = "Contém método para buscar estatisticas de transações")
 @RestController
-@RequestMapping("estatistica")
+@RequestMapping("/estatistica")
 @RequiredArgsConstructor
 public class EstatisticasController {
 
--- src/main/java/com/desafioitau/api_desafio_itau/controller/TransacaoController.java
+++ src/main/java/com/desafioitau/api_desafio_itau/controller/TransacaoController.java
@@ -19,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Transações", description = "Contém operações relativas ao recurso de uma transação")
 @RestController
-@RequestMapping("transacao")
+@RequestMapping("/transacao")
 @RequiredArgsConstructor
 public class TransacaoController {
 
